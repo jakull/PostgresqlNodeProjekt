20fcd729e2ef9732144a2917171e8c16fc150c18
JanK <jakull@web.de>
2024-07-21 17:30:18 +0200

    datbase.config

--- src/config/databaseORM.ts
+++ src/config/databaseORM.ts
@@ -1,15 +1,19 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Angestellte } from "../entity/Angestellte";
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 export const AppDataSource = new DataSource({
     
     type: "postgres", // Typ der Datenbank
-    host: "localhost", // Host der Datenbank
-    port: 5431, // Port der Datenbank
-    username: "postgres", // Dein PostgreSQL-Benutzername
-    password: "postgresuser", // Dein PostgreSQL-Passwort
-    database: "postgres", // Name der Datenbank
+    host: process.env.DB_HOST as string, // Host der Datenbank
+    port: parseInt(process.env.DB_PORT as string, 10), // Port der Datenbank
+    username: process.env.DB_USER as string, // Dein PostgreSQL-Benutzername
+    password: process.env.DB_PASSWORD as string, // Dein PostgreSQL-Passwort
+    database: process.env.DB_NAME as string, // Name der Datenbank
     synchronize: false, // Synchronisiere die Entitäten mit der Datenbank (nicht in Produktion verwenden)
     logging: false, // SQL-Abfragen werden nicht geloggt
     entities: [Angestellte], // Deine Entitäten
